dc155d282e301782c0da6fa5901a834a59f1eb77
ahmad <ezio1497@gmail.com>
2021-04-05 16:45:32 +0300

    Fixed `visualDensity`

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:courtcounter/res/strings.dart';
-
-import 'theme/theme.dart';
 import 'package:flutter/material.dart';
 
+import 'theme/theme.dart';
 import 'ui/main/main_ui.dart';
 
 void main() {
@@ -11,12 +10,10 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: StringRes.app_name,
-      theme: theme,
-      darkTheme: darkTheme,
-      home: MainUI(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        title: StringRes.app_name,
+        theme: theme,
+        darkTheme: darkTheme,
+        home: MainUI(),
+      );
 }
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,9 +1,11 @@
 import 'package:courtcounter/theme/colors.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 final theme = ThemeData.light().copyWith(
   primaryColor: ColorRes.colorPrimary,
   accentColor: ColorRes.colorAccent,
+  visualDensity: kIsWeb? VisualDensity.comfortable : VisualDensity.adaptivePlatformDensity,
   colorScheme: ColorScheme.light(
     primary: ColorRes.colorPrimary,
     secondary: ColorRes.colorAccent,
@@ -14,6 +16,7 @@ final theme = ThemeData.light().copyWith(
 final darkTheme = ThemeData.dark().copyWith(
   primaryColor: ColorRes.colorPrimary,
   accentColor: ColorRes.colorAccent,
+  visualDensity: kIsWeb? VisualDensity.comfortable : VisualDensity.adaptivePlatformDensity,
   colorScheme: ColorScheme.dark(
     primary: ColorRes.colorPrimary,
     secondary: ColorRes.colorAccent,
